80ed99b3172126a075520abb8c04f43c347fd675
CPP-Programming-Exercises <102870893+ys1267@users.noreply.github.com>
2022-04-29 18:11:17 +0900

    week6 assignment2

---
+++ Week6_Homework2/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "Hangman.h"
+#include "MyDic.h"
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+	srand(time(NULL));
+	Hangman game;
+	MyDic hello;
+
+	hello.load("example.txt");
+
+	int random_num = rand() % 8;
+	game.play(hello.getEng(random_num));
+
+
+	cout << endl << "¿µ´Ü¾î : " << hello.getEng(random_num) << endl;
+	cout << "ÇÑ±Û ¼³¸í : " << hello.getKor(random_num) << endl;
+
+	return 0;
+}
