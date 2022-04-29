84ab5ada80b25aee51d5d95cd146ef75bc484514
CPP-Programming-Exercises <102870893+ys1267@users.noreply.github.com>
2022-04-29 15:38:33 +0900

    week4 assignment

---
+++ Week4_Homework/Week4_Homework.cpp
@@ -0,0 +1,12 @@
+﻿#include "Ranking.h"
+extern int PlayFifteenPuzzle();
+
+int main()
+{
+	LoadRanking("ranking.txt");
+	int rank = PlayFifteenPuzzle();
+	PrintRanking();
+	StoreRanking("ranking.txt");
+
+	return 0;
+}
